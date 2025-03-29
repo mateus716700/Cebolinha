1c66c213a4d4352be1161bddc322bf6e27994c72
Mateus Fernades <mateusfernandes131004@gmamil.com>
2025-03-28 22:28:10 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+string nomeDigitado;
+string nomeLeet;
+Console.WriteLine();
+nomeDigitado = Console.ReadLine()!;
+nomeLeet = nomeDigitado.Replace("R","L").
+                        Replace("r","l");
+Console.WriteLine($" {nomeLeet}!");
